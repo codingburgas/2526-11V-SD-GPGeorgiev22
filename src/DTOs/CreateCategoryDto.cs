0bc53b6f4b4cec1da3f8a84409908ee1e491c572
GPGeorgiev22 <GPGeorgiev22@codingburgas.bg>
2026-04-20 19:58:33 +0300

    Add CreateCategory DTO with validation

---
+++ src/DTOs/CreateCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseManagementSchool.DTOs;
+
+public sealed class CreateCategoryDto
+{
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
+    public string Name { get; set; } = string.Empty;
+}
