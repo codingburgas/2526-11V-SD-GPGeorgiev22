68cad9b1166b4af41932097cfd6cea2172a2e7ff
GPGeorgiev22 <GPGeorgiev22@codingburgas.bg>
2026-04-20 19:57:24 +0300

    Add CreateLesson DTO with validation

---
+++ src/DTOs/CreateLessonDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseManagementSchool.DTOs;
+
+public sealed class CreateLessonDto
+{
+    [Required]
+    [StringLength(150, MinimumLength = 3)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(5000, MinimumLength = 10)]
+    public string Content { get; set; } = string.Empty;
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int CategoryId { get; set; }
+}
