008cf59da9e54680b958db2bc62d1e5dfe20e171
GPGeorgiev22 <GPGeorgiev22@codingburgas.bg>
2026-04-20 19:56:14 +0300

    Define password hasher service contract

---
+++ src/Interfaces/IPasswordHasherService.cs
@@ -0,0 +1,7 @@
+﻿namespace WarehouseManagementSchool.Interfaces;
+
+public interface IPasswordHasherService
+{
+    string HashPassword(string password);
+    bool VerifyPassword(string password, string hash);
+}
