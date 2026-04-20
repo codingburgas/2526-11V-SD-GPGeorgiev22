9bf2ffd634a0f75eab595827cc63279d71c1969d
GPGeorgiev22 <GPGeorgiev22@codingburgas.bg>
2026-04-20 20:05:28 +0300

    Add login form view model

---
+++ src/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseManagementSchool.ViewModels;
+
+public sealed class LoginViewModel
+{
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 8)]
+    public string Password { get; set; } = string.Empty;
+}
