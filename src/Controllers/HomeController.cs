17e87b28b226490451ea7bfffd5ddc2cd010b170
GPGeorgiev22 <GPGeorgiev22@codingburgas.bg>
2026-04-20 20:12:24 +0300

    Add HomeController with index test and error actions

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseManagementSchool.Controllers;
+
+public sealed class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Test()
+    {
+        return View();
+    }
+
+    public IActionResult Error()
+    {
+        return View();
+    }
+}
