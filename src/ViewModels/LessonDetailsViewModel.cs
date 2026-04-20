825be75557640f6e7b77f6f2c80fd9ff650bc179
GPGeorgiev22 <GPGeorgiev22@codingburgas.bg>
2026-04-20 20:03:10 +0300

    Add view model for lesson details page

---
+++ src/ViewModels/LessonDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WarehouseManagementSchool.ViewModels;
+
+public sealed class LessonDetailsViewModel
+{
+    public int Id { get; init; }
+    public string Title { get; init; } = string.Empty;
+    public string Content { get; init; } = string.Empty;
+    public string CategoryName { get; init; } = string.Empty;
+}
