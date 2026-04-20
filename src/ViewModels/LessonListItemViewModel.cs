9221c64a4517b9c1ed797a0884a262a22558727d
GPGeorgiev22 <GPGeorgiev22@codingburgas.bg>
2026-04-20 20:02:01 +0300

    Add view model for lesson list items

---
+++ src/ViewModels/LessonListItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WarehouseManagementSchool.ViewModels;
+
+public sealed class LessonListItemViewModel
+{
+    public int Id { get; init; }
+    public string Title { get; init; } = string.Empty;
+    public string CategoryName { get; init; } = string.Empty;
+}
