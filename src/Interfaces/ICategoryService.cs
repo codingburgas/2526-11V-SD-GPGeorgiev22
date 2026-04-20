e1b365c66d4b0f797446598864e5820c5c1a720f
GPGeorgiev22 <GPGeorgiev22@codingburgas.bg>
2026-04-20 19:53:56 +0300

    Define category service contract

---
+++ src/Interfaces/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using WarehouseManagementSchool.DTOs;
+using WarehouseManagementSchool.Models;
+
+namespace WarehouseManagementSchool.Interfaces;
+
+public interface ICategoryService
+{
+    Task<IReadOnlyList<Category>> GetAllAsync();
+    Task<Category?> GetByIdAsync(int id);
+    Task<Category> CreateAsync(CreateCategoryDto dto);
+}
