1328b2ec2c122da242bb3ea399a8f3c799ca349a
GPGeorgiev22 <GPGeorgiev22@codingburgas.bg>
2026-04-20 20:04:19 +0300

    Add register form view model

---
+++ src/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseManagementSchool.ViewModels;
+
+public sealed class RegisterViewModel
+{
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 8)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password))]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
