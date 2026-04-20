bf896fcdcca30390358c4a07cf6f38c90d53c022
GPGeorgiev22 <GPGeorgiev22@codingburgas.bg>
2026-04-20 19:52:47 +0300

    Define lesson service contract

---
+++ src/Interfaces/ILessonService.cs
@@ -0,0 +1,12 @@
+﻿using WarehouseManagementSchool.DTOs;
+using WarehouseManagementSchool.Models;
+
+namespace WarehouseManagementSchool.Interfaces;
+
+public interface ILessonService
+{
+    Task<IReadOnlyList<Lesson>> GetAllAsync();
+    Task<Lesson?> GetByIdAsync(int id);
+    Task<IReadOnlyList<Lesson>> GetByCategoryIdAsync(int categoryId);
+    Task<Lesson> CreateAsync(CreateLessonDto dto);
+}
