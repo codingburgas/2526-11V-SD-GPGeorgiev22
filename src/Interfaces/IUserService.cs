bed401fff0b5e6a1319c71db77014d10389d84a5
GPGeorgiev22 <GPGeorgiev22@codingburgas.bg>
2026-04-20 19:55:05 +0300

    Define user service contract for auth operations

---
+++ src/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using WarehouseManagementSchool.DTOs;
+using WarehouseManagementSchool.Models;
+
+namespace WarehouseManagementSchool.Interfaces;
+
+public interface IUserService
+{
+    Task<User?> GetByUsernameAsync(string username);
+    Task<bool> UsernameExistsAsync(string username);
+    Task<User> RegisterAsync(RegisterUserDto dto);
+    Task<bool> ValidateCredentialsAsync(LoginUserDto dto);
+}
